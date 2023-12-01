2a5b206b68a0ba144df3db7bdad1db7d1e471d7e
rvierbuc <ranlikerats@gmail.com>
2023-12-01 17:16:57 -0600

    Adds database

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { Request, Response } from "express";
 
+const db = require('./db')
 const express = require('express');
 const session = require('express-session');
 
