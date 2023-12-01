2e1c9704dfaa5f910cb3f6c00a7b98ec5f93f720
SNelson723 <stephenn649@gmail.com>
2023-12-01 17:16:53 -0600

    Connected to database

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { Request, Response } from "express";
 
+const db = require('./db/index');
 const express = require('express');
 const session = require('express-session');
 
