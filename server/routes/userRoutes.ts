7918bd788ff826d772f690387db929aaca261a6f
danielalejandrojaen <danielalejandrojaen@gmail.com>
2023-12-04 22:25:28 -0600

    finally fixed it, you can interact with the follower db instance. I tried to show another example with the User instance, but it wasn't working, very confused about that as it is exactly the same in both files. please note that that is still broken but Follower interaction works so definitely a plus

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,8 +1,15 @@
 const express = require('express')
+import { Request, Response } from 'express'
 const router = express.Router()
 const User = require('../../models/user')
 const FollowerModel = require('../../models/follower')
 const Post = require('../../models/post')
+
+
+//Model needs to be created from the database instance
+const FollowerModel = Follower(db, DataTypes);
+//user example
+const UserModel = User(db, DataTypes);
 // ************* GET ROUTES **************
 
 //GET ALL USER POSTS
@@ -19,5 +26,5 @@ router.get('/followingPosts', (req: Request, res: Response) => {
   .then((results: any) => console.log('following results', results))
   .catch((error: Error) => console.log('following results', error))
 
-})
+
 module.exports = router
