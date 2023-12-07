a8ce3360c1e84fcf5c39d56ba0351949e6edd30a
SNelson723 <stephenn649@gmail.com>
2023-12-06 21:00:41 -0600

    Logging data in RecordSynth to check that the data is registering with user interaction

--- client/src/components/synthComponents/AudioTag.tsx
+++ client/src/components/synthComponents/AudioTag.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const AudioTag = ({source}: {source: string}): React.JSX.Element => {
+  const [audioSrc, setAudioSrc] = useState(source);
   console.log('AudioTag', source);
-  let track = document.getElementById('recording');
-  console.log(track);
+  const audioPlayer = useRef<HTMLAudioElement | null>(null);
+  // console.log('audioPlayer', audioPlayer.current?.src)
+  // console.log('AudioPlayer', audioPlayer.current)
   return (
     <div>
-      <audio id="recording" src={source} controls>
+      <audio ref={audioPlayer} id="recording" src={audioSrc} controls>
           Your browser does not support this feature
         </audio>
     </div>
--- client/src/components/synthComponents/RecordSynth.tsx
+++ client/src/components/synthComponents/RecordSynth.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const RecordSynth = ({ audioContext, finalDest, mediaDest, start, stop }: Props) => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [audioSource, setAudioSource] = useState<string>('Howdy');
+  const [audioSource, setAudioSource] = useState<string>('');
   const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
   const audioBuffer = useRef<AudioBufferSourceNode | null>(null);
   const recorder: MediaRecorder = new MediaRecorder(mediaDest.stream);
@@ -36,10 +36,10 @@ const RecordSynth = ({ audioContext, finalDest, mediaDest, start, stop }: Props)
       stop();
       recorder.stop();
       recorder.onstop = async () => {
-        let blob: string = URL.createObjectURL(new Blob(audioChunks, {type: 'audio/wav'}));
+        let blob: string = URL.createObjectURL(new Blob(audioChunks, {type: 'audio/ogg'}));
         setAudioSource(blob.slice(5));
-        console.log(audioSource);
       };
+      console.log('in function', audioSource);
     } catch(error) {
       console.error('Could not stop recording', error);
     }
@@ -51,11 +51,7 @@ const RecordSynth = ({ audioContext, finalDest, mediaDest, start, stop }: Props)
       const formData: FormData = new FormData();
       formData.append('audio', saveBlob);
       const response = await axios.post('/upload', formData);
-      if (response.status === 200) {
-        console.log('Synth saved to cloud');
-      } else {
-        console.error('Error saving synth', response.statusText);
-      }
+      response.status === 200 ? console.log('Synth saved to cloud') : console.error('Error saving synth', response.statusText)
     } catch(error) {
       console.error('Error saving audio', error);
     }
@@ -67,7 +63,12 @@ const RecordSynth = ({ audioContext, finalDest, mediaDest, start, stop }: Props)
       return;
     }
     const audioBlob = new Blob(audioChunks, { type: 'audio/ogg' })
+    console.log('chunk', audioChunks)
+    console.log('blob', audioBlob)
     const arrayBuffer = await audioBlob.arrayBuffer()
+
+    console.log(arrayBuffer);
+    
     audioContext.decodeAudioData(
       arrayBuffer,
       (buffer) => {
@@ -78,6 +79,8 @@ const RecordSynth = ({ audioContext, finalDest, mediaDest, start, stop }: Props)
         audioBuffer.current = audioContext.createBufferSource()
         audioBuffer.current.buffer = buffer
         audioBuffer.current.connect(finalDest)
+
+        console.log(audioBuffer);
 
         audioBuffer.current.onended = () => {
           setIsPlaying(false)
@@ -104,9 +107,9 @@ const RecordSynth = ({ audioContext, finalDest, mediaDest, start, stop }: Props)
         <button onClick={saveRecording}>Save</button>
         <button onClick={playBackRecording}>Playback</button>
       </div>
-      {/* <div>
+      <div>
         <AudioTag source={audioSource} />
-      </div> */}
+      </div>
     </div>
   );
 };
