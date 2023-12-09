596bbe3e12d837fdbeb767024716cc913dac5fb2
danielalejandrojaen <danielalejandrojaen@gmail.com>
2023-12-08 18:30:43 -0600

    Added current user loader for user profile

--- client/src/components/userProfile.tsx
+++ client/src/components/userProfile.tsx
@@ -3,13 +3,19 @@ import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
 
 const UserProfile = () => {
-    const [user, setUser] = useState({});
-    // const userLoader = useLoaderData();
-    // console.log('userLoader');
+    const [selectedUser, setselectedUser] = useState({});
+    const currentUser: any = useLoaderData();
+    console.log('userLoader', currentUser);
     return (
         <div className="user-main">
-            <h1>test</h1>
-          
+            <div className="user-profile-card">
+                <div className="user-profile-image">
+                    <img src={currentUser.profileImgUrl} alt="user profile image" />
+                </div>
+                <div className="user-profile-info">
+                    <h1>{currentUser.username}</h1>
+                </div>
+            </div>
         </div>
     )
 };
