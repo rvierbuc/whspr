f21b6fba5254f08d83e3636213b9fe939ff24757
danielalejandrojaen <danielalejandrojaen@gmail.com>
2023-12-10 16:08:05 -0600

    adjusted some user profile stuff

--- client/src/components/userProfile.tsx
+++ client/src/components/userProfile.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import CardTitle from 'react-bootstrap/esm/CardTitle';
 import Post from './Post';
 import WaveSurferComponent from './WaveSurfer';
 const UserProfile = ({audioContext}) => {
@@ -24,14 +26,27 @@ const UserProfile = ({audioContext}) => {
       }, [])
     return (
         <div className="user-main">
-            <div className="user-profile-card">
+            <Card style={{ width: '20rem' }}>
+                <div className="user-main-card">
+                    <div className="user-pfp">
+                        <img src={currentUser.profileImgUrl} alt="user profile image" />
+                    </div>
+                </div>
+                <Card.Body>
+                    <Card.Title>{currentUser.username}</Card.Title>
+                    <Card.Text>
+                        {currentUser.bio || 'No bio yet'}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            {/* <div className="user-profile-card">
                 <div className="user-profile-image">
                     <img src={currentUser.profileImgUrl} alt="user profile image" />
                 </div>
                 <div className="user-profile-info">
                     <h1 style={{color: 'white'}}>{currentUser.username}</h1>
                 </div>
-            </div>
+            </div> */}
             {selectedUser.length > 0 ? selectedUser.map((post) => (
             <div>
             <WaveSurferComponent postObj={post} audioUrl={post.soundUrl} postId={post.id} />
