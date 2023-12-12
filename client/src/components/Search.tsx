2032a421ce332b08785b8840568d1ca80e8363e4
danielalejandrojaen <danielalejandrojaen@gmail.com>
2023-12-12 17:02:34 -0600

    Have basic algolia instant search working with my search_index, going to flesh out the search

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,8 +1,31 @@
 import algoliasearch from 'algoliasearch/lite';
 import React from 'react';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch';
+import { InstantSearch, SearchBox, Hits, Configure } from 'react-instantsearch';
+// require('dotenv').config();
+import { v4 as uuidv4 } from 'uuid';
 
-const searchClient = algoliasearch('APP ID', 'Admin API Key');
+const generateUserToken = (): string => {
+  return uuidv4();
+};
+
+// TODO:
+interface EnvironmentVariables {
+  APP_ID: string;
+  ADMIN_API_KEY: string;
+}
+// const { APP_ID, ADMIN_API_KEY }: EnvironmentVariables = process.env as any;
+
+
+
+const userToken = generateUserToken();
+
+const searchClient = algoliasearch('2580UW5I69', 'b0f5d0cdaf312c18df4a45012c4251e4', {
+  headers: {
+    'X-Algolia-UserToken': userToken,
+  }
+});
+
+
 function Hit({ hit }) {
   return (
     <article>
@@ -13,9 +36,10 @@ function Hit({ hit }) {
 }
 const Search = () => {
   return (
-    <InstantSearch searchClient={searchClient} indexName="user_index">
+    <InstantSearch searchClient={searchClient} indexName="search_index">
       <SearchBox />
       <Hits hitComponent={Hit} />
+      <Configure userToken={userToken} />
     </InstantSearch>
   );
 }
--- server/algolia.ts
+++ server/algolia.ts
@@ -1,10 +1,17 @@
+// require('dotenv').config();
 // hello_algolia.js
 import algoliasearch from 'algoliasearch';
 import { User } from './dbmodels'
 //const axios = require('axios');
 // Connect and authenticate with your Algolia app
 //TO DO:
-const client = algoliasearch('APP ID', 'Admin API Key');
+// interface EnvironmentVariables {
+//   APP_ID: string;
+//   ADMIN_API_KEY: string;
+// }
+// const { APP_ID, ADMIN_API_KEY }: EnvironmentVariables = process.env as any;
+
+const client = algoliasearch('2580UW5I69', 'b0f5d0cdaf312c18df4a45012c4251e4');
 
 
 // Create a new index and add a record
@@ -45,7 +52,47 @@ const getUserData = async () => {
 }
 getUserData()
 // Search the index and print the results
-index
-  .search('Sydney')
-  .then(({ hits }) => console.log(hits[0]))
-  .catch((error) => console.log(error))
+// index
+//   .search('Sydney')
+//   .then(({ hits }) => console.log('then', hits)) //this is not working right now 
+//   .catch((error) => console.log('catch', error))
+
+const searchIndex = client.initIndex('search_index');
+
+const getSearchData = async () => {
+  try{ 
+    const allUserData = await User.findAll({})
+    const mappedUserData = allUserData.map((user) => user.dataValues)
+
+        mappedUserData.map(async (record) =>{
+          try{
+            await searchIndex.saveObject(record, {
+              autoGenerateObjectIDIfNotExist: true
+            })
+          }catch(error){
+            console.log('save obj', error)
+          }
+      })
+
+  }catch(error){
+    console.log('error', error)
+  }
+}
+getSearchData();
+
+searchIndex.setSettings({
+  searchableAttributes: [
+    'username',
+  ]
+}).then(() => {
+  console.log('set settings')
+}).catch((error) => {
+  console.log('error', error)
+})
+searchIndex
+.search('Daniel')
+.then(({ hits }) => console.log('then', hits)) //this is not working right now
+.catch((err) => console.log('catch', err));
+
+
+
