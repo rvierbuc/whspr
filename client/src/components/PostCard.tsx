006ee9936f47c90ded0756f9c13a8781b97a0ec7
danielalejandrojaen <danielalejandrojaen@gmail.com>
2023-12-15 10:54:47 -0600

    Added seach component to the postcard component instead of record post

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -1,6 +1,50 @@
 import React, { useState } from 'react'
 import { RecordPost } from './RecordPost'
 import { useLoaderData } from 'react-router-dom'
+import algoliasearch from 'algoliasearch';
+import { InstantSearch, SearchBox, Hits, useSearchBox, useHits, Configure } from 'react-instantsearch';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const generateUserToken = (): string => {
+  return uuidv4();
+};
+
+const userToken = generateUserToken();
+
+const searchClient = algoliasearch('2580UW5I69', 'b0f5d0cdaf312c18df4a45012c4251e4', {
+  headers: {
+    'X-Algolia-UserToken': userToken,
+  }
+});
+
+const Hit = ({ hit }) => {
+  const { hits } = useHits(); // the array of hits
+  // console.log('hits', hits); //the individual hit obj
+  return (
+    <article>
+      {hit.category}
+    </article>
+  )};
+const CategorySearch = () => {  
+  const [currentSearch, setCurrentSearch] = useState<string>('');
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentSearch(event.target.value);
+  }
+  return (
+    <div>
+      <InstantSearch 
+      searchClient={searchClient} 
+      indexName="category_index"
+      initialUiState={{ searchBox: { query: currentSearch } }}
+      >
+        <SearchBox onInput={handleSearchChange} />
+        {currentSearch && <Hits hitComponent={Hit} />}
+        <Configure userToken={userToken} />
+      </InstantSearch>
+    </div>
+  );
+}
 
 const PostCard = ({ audioContext }: { audioContext: BaseAudioContext }) => {
   const [postCreated, setPostCreated] = useState(false)
@@ -33,6 +77,7 @@ const PostCard = ({ audioContext }: { audioContext: BaseAudioContext }) => {
           onChange={(e) => { setTitle(e.target.value) }}
           className='input-control'
           />
+          <CategorySearch />
           </div>
 <RecordPost
 user={user}
