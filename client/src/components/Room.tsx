1dd01bed1a90fbb4dbff52511fbcef6e53e66533
rvierbuc <ranlikerats@gmail.com>
2024-01-23 16:52:06 -0600

    may fix user render bug

--- client/src/components/Room.tsx
+++ client/src/components/Room.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEventHandler, MutableRefObject, useEffect, useRef, useState } from 'react';
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Peer from 'peerjs';
 import AgoraRTC from 'agora-rtc-sdk';
 // import agoraConfig from '../agoraConfig'
@@ -12,9 +12,9 @@ import RoomUser from './RoomUser'
 
 
 
-const Room = ({  host, id,  audioContext }) => {
-  
-  const {name} = useParams()
+const Room = ({ host, id, audioContext }) => {
+
+  const { name } = useParams()
   const [channelName, setChannelName] = useState("a");
   const [channel, setChannel] = useState<string>()
   const [uid, setUid] = useState<number>();
@@ -27,7 +27,7 @@ const Room = ({  host, id,  audioContext }) => {
   const navigate = useNavigate()
   const user: any = useLoaderData();
 
-  
+
   useEffect(() => {
     setRoomProps()
     const initUserMedia = async () => {
@@ -36,45 +36,47 @@ const Room = ({  host, id,  audioContext }) => {
         console.log('channel', channel, owner)
         const userMediaStream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
         setStream(userMediaStream);
-        
+
         if (user.username !== owner) {
           //setListeners([...listeners, user]);
         }
-        socket.emit('sendUser', {user: user, stream: userMediaStream})
-        socket.on('recieveUser', (user) => {
-          console.log('list', owner, user)
-          if(listeners !== undefined && listeners.length > 0 && user.username !== owner.username){
-            setListeners((prevListeners) => [...prevListeners, user])
-            
-          }else{
-            setListeners([user])
-            console.log('zero', listeners)
-          }
-        })
+        socket.emit('sendUser', { user: user, stream: userMediaStream })
+        socket.on('recieveUser', (receivedUser) => {
+          console.log('list', owner, receivedUser)
+          setListeners((prevListeners) => {
+            const isUserAlreadyListener = prevListeners.some(user => user.username === receivedUser.username);
+            if (!isUserAlreadyListener && receivedUser.username !== owner.username) {
+              return [...prevListeners, receivedUser];
+            } else {
+              return prevListeners;
+            }
+          });
+        });
+
         createChannel(channelName, uid, '007eJxTYOj8fWnTO/mg+TlWgcG1EboX11Xm+Mp+e3boR9WcPa0LlpgrMCQaJyUbmJgmWphZWphYmJtbGKUYpJoZJhmbmllYWpoalUlvSG0IZGQ4fsmflZEBAkF8RoZEBgYAYqUfTQ==', userMediaStream);
       } catch (error) {
         console.log('Error initializing user media:', error);
       }
     };
-    
+
     initUserMedia();
-    
+
     subscribeRemoteUser((user, mediaType) => {
       if (mediaType === 'audio') {
         const remoteAudioTrack = user.audioTrack;
         user.audioTrack.play();
         setRemoteAudioTracks((prevTracks) => [...prevTracks, remoteAudioTrack]);
-        
+
         remoteAudioRef.current = remoteAudioTrack;
       }
     });
 
-    
+
   }, [audioContext]);
-  
+
   const setRoomProps = async () => {
-    try{
-    
+    try {
+
       const info = await axios.get(`/post/radio/${name}`)
       const ost = info.data[0].host
       const hosts = await axios.get(`/post/user/${ost}`)
@@ -83,7 +85,7 @@ const Room = ({  host, id,  audioContext }) => {
       setUid(info.data[0].id)
       setOwner(hosts.data[0])
 
-    }catch{
+    } catch {
       console.log('no')
     }
   }
@@ -103,107 +105,107 @@ const Room = ({  host, id,  audioContext }) => {
     leaveChannel();
     //console.log('stream', stream);
     stopAudio();
-    if(user.username === owner.username){
+    if (user.username === owner.username) {
       axios.delete(`/post/radio/${user.username}`)
-      .then(() => {
-        console.log('done')
-      }).catch(() => {
-        console.log('uhh')
-      })
+        .then(() => {
+          console.log('done')
+        }).catch(() => {
+          console.log('uhh')
+        })
     }
     window.location.href = '/protected/radio'
   };
-  
-  return (
-    
-    <div>
-      <div style={{justifyContent: 'right', flexDirection: 'row', display: 'flex', alignItems: 'right'}}>
 
-      <img
-            onClick={handleLeaveChannel}
-            src={require('../style/icons8-leave-32.png')}
-            style={{width: '50px', margin: '20px', justifyContent: 'right'}}
-          />
+  return (
+
+    <div>
+      <div style={{ justifyContent: 'right', flexDirection: 'row', display: 'flex', alignItems: 'right' }}>
+
+        <img
+          onClick={handleLeaveChannel}
+          src={require('../style/icons8-leave-32.png')}
+          style={{ width: '50px', margin: '20px', justifyContent: 'right' }}
+        />
       </div>
 
-    <div style={{ alignItems: 'center'}} className='container'>
-   
-      <h1 style={{color: 'white'}}>{channel}</h1>
+      <div style={{ alignItems: 'center' }} className='container'>
 
-      
-      <div className='card'>
-
-      <audio ref={remoteAudioRef} autoPlay />
+        <h1 style={{ color: 'white' }}>{channel}</h1>
 
 
-       
-       <div className='room-info'>
-       {owner ? <img className="rounded-circle"
-                    style={{
-                      width: 'auto',
-                      height: '100px',
-                      marginTop:  '40px',
-                      marginLeft: '40px',
-                      objectFit: 'scale-down',
-                      borderStyle: 'solid',
-                      borderWidth: 'medium',
-                      borderColor: '#3C3556',
-                    }}  src={owner.profileImgUrl} alt="user profile image" />
-       :
-          <img style={{marginLeft: '15px', marginTop: '15px'}} width="100" src="https://lh3.googleusercontent.com/a/ACg8ocI6UOrLKNPeKzMpAobwFfMo2jVBc2SccK66hzTPMkEk=s96-c" alt="user profile image" />
-       }
-          <AudioAnalyser audioContext={audioContext} audioStream={stream}/>
-         
-       {mute ?  <img
-            onClick={muted}
-            src={require('../style/mute.svg')}
-            style={{width: '50px', margin: '20px', justifyContent: 'right'}}
-          /> :<img
-          onClick={muted}
-          src={require('../style/unmute.svg')}
-          style={{width: '50px', margin: '20px', justifyContent: 'right'}}
-        />}
+        <div className='card'>
 
-       </div>
-      
-      <br />
-      {/* <button
+          <audio ref={remoteAudioRef} autoPlay />
+
+
+
+          <div className='room-info'>
+            {owner ? <img className="rounded-circle"
+              style={{
+                width: 'auto',
+                height: '100px',
+                marginTop: '40px',
+                marginLeft: '40px',
+                objectFit: 'scale-down',
+                borderStyle: 'solid',
+                borderWidth: 'medium',
+                borderColor: '#3C3556',
+              }} src={owner.profileImgUrl} alt="user profile image" />
+              :
+              <img style={{ marginLeft: '15px', marginTop: '15px' }} width="100" src="https://lh3.googleusercontent.com/a/ACg8ocI6UOrLKNPeKzMpAobwFfMo2jVBc2SccK66hzTPMkEk=s96-c" alt="user profile image" />
+            }
+            <AudioAnalyser audioContext={audioContext} audioStream={stream} />
+
+            {mute ? <img
+              onClick={muted}
+              src={require('../style/mute.svg')}
+              style={{ width: '50px', margin: '20px', justifyContent: 'right' }}
+            /> : <img
+              onClick={muted}
+              src={require('../style/unmute.svg')}
+              style={{ width: '50px', margin: '20px', justifyContent: 'right' }}
+            />}
+
+          </div>
+
+          <br />
+          {/* <button
       type="button"
       className='btn btn-dark'
     onClick={handleLeaveChannel}>Leave Channel</button> */}
-      </div>
+        </div>
 
-      <div style={{ marginTop: '20px',  alignItems: 'center'}} className='container'>
+        <div style={{ marginTop: '20px', alignItems: 'center' }} className='container'>
 
-        
+
           <div>
-            { listeners ?
-            listeners.map((listener) => {
-              return (
-                <div>
-                  {listener.user.username !== owner.username ? 
-                  <RoomUser image={listener.user.profileImgUrl} audioStream={listener.stream} audioContext={audioContext} muted={muted}/>
-                  : <br/>
-                  }
+            {listeners ?
+              listeners.map((listener) => {
+                return (
+                  <div>
+                    {listener.user.username !== owner.username ?
+                      <RoomUser image={listener.user.profileImgUrl} audioStream={listener.stream} audioContext={audioContext} muted={muted} />
+                      : <br />
+                    }
                   </div>
-             // <img style={{margin: '15px'}} width="100" src={listener.profileImgUrl} alt="user profile image" />)
-        //       {mute ?  <button
-        //         type="button"
-        //         style={{margin: '15px'}}
-        // className='btn btn-dark'
-        // onClick={() => {muted()}}
-        // >Unmute</button> : <button
-        // type="button"
-        // style={{margin: '15px'}}
-        // className='btn btn-light'
-        // onClick={() => {muted()}}
-        // >Mute</button>}
-              )
-            
-        
-      }) : <br></br>
-    }
-          {/* <img style={{margin: '15px'}} width="100" src="https://lh3.googleusercontent.com/a/ACg8ocI6UOrLKNPeKzMpAobwFfMo2jVBc2SccK66hzTPMkEk=s96-c" alt="user profile image" />
+                  // <img style={{margin: '15px'}} width="100" src={listener.profileImgUrl} alt="user profile image" />)
+                  //       {mute ?  <button
+                  //         type="button"
+                  //         style={{margin: '15px'}}
+                  // className='btn btn-dark'
+                  // onClick={() => {muted()}}
+                  // >Unmute</button> : <button
+                  // type="button"
+                  // style={{margin: '15px'}}
+                  // className='btn btn-light'
+                  // onClick={() => {muted()}}
+                  // >Mute</button>}
+                )
+
+
+              }) : <br></br>
+            }
+            {/* <img style={{margin: '15px'}} width="100" src="https://lh3.googleusercontent.com/a/ACg8ocI6UOrLKNPeKzMpAobwFfMo2jVBc2SccK66hzTPMkEk=s96-c" alt="user profile image" />
        {mute ?  <button
         type="button"
         style={{margin: '15px'}}
@@ -217,15 +219,15 @@ const Room = ({  host, id,  audioContext }) => {
         >Mute</button>} */}
 
 
-        
-          
+
+
+          </div>
+
+
         </div>
 
-        
-      </div>
 
-      
-    </div>
+      </div>
     </div>
   );
 };
